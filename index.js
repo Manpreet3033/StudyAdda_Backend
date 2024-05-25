c036cd76024d9a31aca16e1d612967c8aa2fe5fa
Manpreet3033 <manpreet0855.be21@chitkara.edu.in>
2024-05-25 14:53:07 +0530

    Updated Vercel.json

--- index.js
+++ index.js
@@ -21,12 +21,7 @@ database.connect();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "https://study-adda-edtech.vercel.app",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(
   fileUpload({
